cce8450c046e3fc0f4c22870583de6e12a036a18
Roman Ohiienko <ohiienko.r@gmail.com>
2025-10-21 22:42:28 +0300

    fix: attempt to fix 405 error

--- app/api/bot/route.ts
+++ app/api/bot/route.ts
@@ -3,12 +3,17 @@ import { bot } from "@/lib/bot";
 
 const handleUpdate = webhookCallback(bot, "std/http");
 
-export const POST = async (req: Request) => {
+export async function POST(request: Request) {
   try {
-    const res = await handleUpdate(req);
-    return res;
+    return await handleUpdate(request);
   } catch (err) {
     console.error("Webhook error:", err);
-    return new Response("Error", { status: 500 });
+    return new Response("Internal Server Error", { status: 500 });
   }
-};
+}
+
+export function GET() {
+  return new Response("This endpoint is for Telegram Webhook (POST only).", {
+    status: 405,
+  });
+}
